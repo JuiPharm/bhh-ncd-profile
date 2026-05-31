7f41deb1ad06c7fa16e16f07f786b3eb698a0da7
JuiPharm <jui_3065@hotmail.com>
2026-05-31 13:16:47 +0700

    Update GAS_WEB_APP_URL in config.js

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 // BHH NCD Profile - Development Runtime Configuration
 window.BHH_CONFIG = {
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwZMd0TECf-rzl9WiNd2OKRPTRcTaxwMwJZORbHDufY3eIsd1fJzvygLqG-xLQSeJkm8A/exec",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwDXHKA7Zb4GkTtEDbVJzP18v-X0Z1y_sXwoW_SeEj73E2P6OMEdilBsTRJy1BjyBUq2g/exec",
   APP_NAME: "BHH NCD Profile",
   APP_VERSION: "1.0.0"
 };
