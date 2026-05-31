267c8ce0a9ea412c8f494c6dd44a13ae2ef00eb6
JuiPharm <jui_3065@hotmail.com>
2026-05-31 15:55:07 +0700

    fix dashboard missing cartesian grid

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 // BHH NCD Profile - Development Runtime Configuration
 window.BHH_CONFIG = {
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwDXHKA7Zb4GkTtEDbVJzP18v-X0Z1y_sXwoW_SeEj73E2P6OMEdilBsTRJy1BjyBUq2g/exec",
+  GAS_WEB_APP_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
   APP_NAME: "BHH NCD Profile",
   APP_VERSION: "1.0.0"
 };
